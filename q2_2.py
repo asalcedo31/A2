7db2f6f6189b26922d788fc46d6505721507b017
asalcedo31 <adrisal31@gmail.com>
2017-11-10 00:41:22 -0500

    rough 2.2 done

--- q2_2.py
+++ q2_2.py
@@ -6,6 +6,7 @@ Here you should implement and evaluate the Conditional Gaussian classifier.
 
 import data
 import numpy as np
+import math as math
 # Import pyplot - plt.imshow is useful!
 import matplotlib.pyplot as plt
 
@@ -61,7 +62,7 @@ def compute_sigma_mles(train_data, train_labels, means):
         print(obs_diff[0:3,])
      #   cov = np.cov(class_data,rowvar=False)
         cov = np.dot(obs_diff,obs_diff.transpose())/class_data.shape[0]
-        cov = cov + 0.01
+        cov = cov + np.identity(64)*0.01
      ##   print(cov.shape)
         covariances[k-1]=cov
             
@@ -92,7 +93,7 @@ def generative_likelihood(digits, means, covariances):
     '''
     return None
 
-def conditional_likelihood(digits, means, covariances):
+def conditional_likelihood(digits, means, covariances,labels=None):
     '''
     Compute the conditional likelihood:
 
@@ -101,7 +102,58 @@ def conditional_likelihood(digits, means, covariances):
     This should be a numpy array of shape (n, 10)
     Where n is the number of datapoints and 10 corresponds to each digit class
     '''
-    return None
+    prior = 1/10
+    
+    const1 = (2*math.pi)**(-covariances[0].shape[0]/2)
+    print(const1)
+    cond_lik = np.zeros((digits.shape[0],1))
+    for i in range(digits.shape[0]):
+        data = digits[i,:]
+        evidence = np.zeros((10,1))
+        reshaped = data.reshape(8,8)
+        if (labels != None):
+            lab = int(labels[i])
+       #     print("lab: ", lab)
+        else:
+            lab = None
+        for k in range(10):
+    #        print("class: ", k)
+            class_mean = means[k,:]
+    #        print("means", class_mean.shape)
+            det = np.linalg.det(covariances[k])
+            const2 = 1/math.sqrt(det)
+  #          print("const2:", const2)
+            
+       #     print("data", data.shape)
+        #    print("data", data.shape)
+            diff = data-class_mean
+            cov_inv = np.linalg.inv(covariances[k])
+       #     print("diff:", min(diff),max(diff))
+       ##     print("inv ",cov_inv)
+            in_exp = -1/2*np.dot(np.dot(diff.transpose(),cov_inv),diff)
+        #    print("exp",math.exp(in_exp))
+            likelihood = const1*const2*math.exp(in_exp)
+        #    print("likelihood: ", likelihood)
+            evidence[k]=likelihood
+       
+        cond_lik_data = prior*evidence/np.sum(evidence,axis=0)
+  #      print(cond_lik_data)
+   #     plt.imshow(reshaped,cmap='gray')
+    #    plt.show()
+        if (lab == None):
+            best_lik = np.max(cond_lik_data)
+            class_pred = np.where(cond_lik_data==best_lik)[0]+1
+            if (class_pred==10):
+                class_pred = 0
+            cond_lik[i] = class_pred
+        else:
+            best_lik = cond_lik_data[lab-1]
+            cond_lik[i] = np.log(best_lik)
+    #    print("best:",best_lik,np.where(cond_lik_data==best_lik)[0]+1, np.log(best_lik))
+        
+            
+        
+    return cond_lik
 
 def avg_conditional_likelihood(digits, labels, means, covariances):
     '''
@@ -111,12 +163,22 @@ def avg_conditional_likelihood(digits, labels, means, covariances):
 
     i.e. the average log likelihood that the model assigns to the correct class label
     '''
-    cond_likelihood = conditional_likelihood(digits, means, covariances)
+    cond_likelihood = conditional_likelihood(digits, means, covariances,labels)
+    print("cond like shape",cond_likelihood.shape)
+    avg = np.sum(cond_likelihood)/cond_likelihood.shape[0]
 
     # Compute as described above and return
-    return None
+    return avg
 
-def classify_data(digits, means, covariances):
+def classification_accuracy(predicted_labels,eval_labels):
+    pred_truth = np.zeros(predicted_labels.shape[0],dtype={'names':('predicted','truth'),'formats':('int8','int8')})
+    pred_truth['predicted'] = predicted_labels[:,0]
+    pred_truth['truth']= eval_labels
+    print(pred_truth[0:3])
+    correct = pred_truth[pred_truth['predicted'] == pred_truth['truth']]
+    return(correct.shape[0]/pred_truth.shape[0])
+
+def classify_data(digits, means, covariances,labels):
     '''
     Classify new points by taking the most likely posterior class
     '''
@@ -133,6 +195,21 @@ def main():
   #  print(means)
     covariances = compute_sigma_mles(train_data, train_labels,means)
     plot_cov_diagonal(covariances)
+ #   avg_train = avg_conditional_likelihood(train_data,train_labels,means,covariances)
+  #  print("avg_train", avg_train)
+ #   avg_test = avg_conditional_likelihood(test_data,test_labels,means,covariances)
+#    print("avg_test", avg_test)
+    pred_train = conditional_likelihood(train_data,means,covariances)
+    acc_train = classification_accuracy(pred_train,train_labels)
+    
+    pred_test = conditional_likelihood(test_data,means,covariances)
+    print(pred_test[0:5])
+    acc_test = classification_accuracy(pred_test,test_labels)
+    print("train: ", acc_train, "test: ", acc_test)
+#    print(acc_test)
+   # conditional_likelihood(train_data,means,covariances)
+    
+    #conditional_likelihood(train_data,means,covariances,train_labels)
     # Evaluation
 
 if __name__ == '__main__':
