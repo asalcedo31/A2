added normalization, fixed CV

q2_1.py
@@ -10,6 +10,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import KFold
 import pandas as pd
+from sklearn import preprocessing
 
 class KNearestNeighbor(object):
     '''
@@ -45,29 +46,26 @@ class KNearestNeighbor(object):
 
         You should return the digit label provided by the algorithm
         self 
+        test point ia a 1 x 64 numpy array of a new observation to classify
+        k is the number of closest trianing points that vote on the class for the test point
         '''
+        #get distance to the training points
         dist = self.l2_distance(test_point)
         labeled_dist = np.zeros(self.train_labels.shape[0],dtype={'names':('labels','dist'), 'formats':('int8','f4')})
         labeled_dist['labels']= self.train_labels
         labeled_dist['dist'] = dist
+        #sort by distance
         labeled_dist = np.sort(labeled_dist,order='dist')
-     #   print(labeled_dist[0])
+     #   if k = 1 just get the closest test point and return
         if k == 1:
             digit = labeled_dist['labels'][0]     
             return(digit)
-        top_k_lab = labeled_dist['labels'][0:k]
-     #   print(top_k_lab)    
-        if k == 1:
-            digit = labeled_dist['labels':0]        
+        # if k >1 then get the top k points and count their class labels
+        top_k_lab = labeled_dist['labels'][0:k]  
         unique_counts = np.asarray(np.unique(top_k_lab,return_counts=True))
-      #  unique_counts = np.sort(unique_counts.T,axis=1)
-   #     print(unique_counts)
-     #   print(np.ndarray.flatten(np.argwhere(unique_counts[1,:] == np.amax(unique_counts[1,:]))))
+         #   Assign the test point the class with the most votes, if tie then choose randomly from the tied classes
         top_counts = unique_counts[:,np.random.choice(np.ndarray.flatten(np.argwhere(unique_counts[1,:] == np.amax(unique_counts[1,:]))),size=1)]
-       # print(top_counts.shape)
-   #     print(top_counts)
-     #   if top_counts.shape[0] > 1:
-            
+           
         digit = top_counts[0]
         return digit
 
@@ -78,10 +76,16 @@ def cross_validation(train_data, train_labels, k_range=np.arange(1,16),folds=10)
     Note: Previously this function took knn as an argument instead of train_data,train_labels.
     The intention was for students to take the training data from the knn object - this should be clearer
     from the new function signature.
+    train data is an n x 64 numpy array of observations
+    train labels is a n x 1 array of labels for each observations
+    k range is a tuple of the minimum and maximum+1 values of k to test
+    folds is the number of fold to use
     '''
+    #split data
     kf = KFold(n_splits=folds)
     k_acc = np.zeros((k_range.shape[0],folds))
     f = 0;
+    # Loop over folds
     for train_index,test_index in kf.split(train_data):
         x_f_train, x_f_test = train_data[train_index], train_data[test_index]
         y_f_train, y_f_test = train_labels[train_index], train_labels[test_index]
@@ -90,59 +94,83 @@ def cross_validation(train_data, train_labels, k_range=np.arange(1,16),folds=10)
         for k in k_range:
             k_acc[k-1,f-1]=classification_accuracy(knn_f,k,x_f_test,y_f_test)
         f +=1
-        # Loop over folds
-        # Evaluate k-NN
-        # ...
-   # print(k_acc)
-    k_acc_mean = np.mean(k_acc,axis=0)
-#    print(k_acc)
-   # print(np.argmin(k_acc_mean))
-    best_k = np.argmin(k_acc_mean)+1
+        
+    # obtain accuracy across folds
+    k_acc_mean = np.mean(k_acc,axis=1)
+   # best k has the highest accuracy
+    best_k = np.argmax(k_acc_mean)+1
     print("best k", best_k)
     acc_df = pd.DataFrame({'k': k_range, 'Accuracy':k_acc_mean})
     acc_df = acc_df[['k','Accuracy']]
     acc_df_tex = acc_df.to_latex(index=False)
-    print(acc_df_tex)
+    print(acc_df)
     return(best_k)
 
 def classification_accuracy(knn, k, eval_data, eval_labels):
     '''
     Evaluate the classification accuracy of knn on the given 'eval_data'
     using the labels
+    knn is a knn object
+    k is the desired number of k to use in the knn classifier
+    eval data is a n x 64 numpy matrix of observations to classify
+    eval_labels is a n x 1 vector of true labels
     '''
+    # obtain predictions from knn classifier
     predicted_labels=knn_classify(knn,eval_data,k)
+    # combine prediction into a single array
     pred_truth = np.zeros(predicted_labels.shape[0],dtype={'names':('predicted','truth'),'formats':('int8','int8')})
     pred_truth['predicted'] = predicted_labels
     pred_truth['truth']= eval_labels
-   # print(pred_truth.shape)
+    #count the rows where predicted and true labels match
     correct = pred_truth[pred_truth['predicted'] == pred_truth['truth']]
     return(correct.shape[0]/pred_truth.shape[0])
 
 def knn_classify(knn,test_data,k):
+    '''
+    Assigns a label to test data using a knn model
+    knn is a knn object
+    test_data is a n x 64 numpy array of observations
+    k is the desired k to use to assign labels in the knn object
+    '''
     predicted_labels=np.zeros(test_data.shape[0])
+    #iterate over test data
     for i in np.arange(0,test_data.shape[0]-1):
         predicted_labels[i]=knn.query_knn(test_data[i],k)
     return(predicted_labels)
-
+def normalize(data,scaler=None): 
+   '''
+   normalize features so noisy features don't 
+   influence results. If a scaling object is provided it will use it to scale, otherwise it will
+   scale based on the mean and variance of the input dataset.
+   Inputs: dataset is a N x k matrix to scale
+   scaler: a sklearn standard scaler object
+   Outputs: scaled data, if scaler not provided,  also outputs the sklearn scaler object used to scale the data
+   '''
+   if scaler == None:
+        #scale data and save scaling parameters
+        scaler = preprocessing.StandardScaler().fit(data)
+        return(scaler.transform(data),scaler)
+   else:  #if using input scaling
+        print("using input scaling\n")
+        return(scaler.transform(data))
+    
 def main():
     train_data, train_labels, test_data, test_labels = data.load_all_data('data')
+    train_data, scaler = normalize(train_data)
+    test_data = normalize(test_data,scaler)
     knn = KNearestNeighbor(train_data, train_labels)
-  #  np.random.seed()
-    # Example usage:
-    predicted_label = knn.query_knn(test_data[0], 2)
-    print("predicted_label:",predicted_label)
-    predicted_label = knn.query_knn(test_data[9], 2)
-   # print("predicted_label:",predicted_label)
-   # accuracy_test_k1 = classification_accuracy(knn,1,test_data,test_labels)
-   # accuracy_train_k1 = classification_accuracy(knn,1,train_data,train_labels)
-   # accuracy_test_k15 = classification_accuracy(knn,15,test_data,test_labels)
-   # accuracy_train_k15 = classification_accuracy(knn,15,train_data,train_labels)
-   # print("test k1:", accuracy_test_k1, "train k1:", accuracy_train_k1, "test k15:", accuracy_test_k15, "train k15:", accuracy_train_k15)
-   
-    best_k = cross_validation(train_data,train_labels,np.arange(1,3),2)
+    # accuracy for train and test data when k=1 and k=15
+    accuracy_test_k1 = classification_accuracy(knn,1,test_data,test_labels)
+    accuracy_train_k1 = classification_accuracy(knn,1,train_data,train_labels)
+    accuracy_test_k15 = classification_accuracy(knn,15,test_data,test_labels)
+    accuracy_train_k15 = classification_accuracy(knn,15,train_data,train_labels)
+    print("test k1:", accuracy_test_k1, "train k1:", accuracy_train_k1, "test k15:", accuracy_test_k15, "train k15:", accuracy_train_k15)
+   #find the optimal k with cross valudation 
+    best_k = cross_validation(train_data,train_labels,np.arange(1,16),10)
+    #report testing and training accuracy for the best k
     accuracy_test_best = classification_accuracy(knn,best_k,test_data,test_labels)
     accuracy_train_best = classification_accuracy(knn,best_k,train_data,train_labels)
-    print("test k1:", accuracy_test_best, "train k1:", accuracy_train_best)
+    print("test k best:", accuracy_test_best, "train k best:", accuracy_train_best)
   #  print(predicted_labels[1:5])
 
 if __name__ == '__main__':